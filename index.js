fa3c014791ab93e35ff39ac888233726ef7e78a1
Samuelfa <samuelfaalmeida2008@gmail.com>
2025-04-01 20:57:41 -0300

    Converter add

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 function conversor (){ 
+
+    CotaWonsBR    =  0.0040;
+    CotaWonsEUA   =  0.00068;
+    CotaWonsEuro  =  0.00063;
+ 
+
    Nwons = prompt("Quanto Wons você deseja converter?");
-   CotaWonsBR = 	0.0040;
-   CotaWonsEUA =  0.00068;
-   BRL = Nwons * CotaWonsBR ; 
-   Dolar =  Nwons * CotaWonsEUA ;
-   alert ("Seu número de WONS em BRL R$" + BRL + "    Seu número de WONS em USD $" + Dolar);
+
+
+   BRL   =    Nwons * CotaWonsBR ; 
+   USD   =    Nwons * CotaWonsEUA ;
+   EUR   =    Nwons * CotaWonsEuro ;
+
+   alert ("Seu número de WONS em BRL R$" + BRL); 
+   alert ("Seu número de WONS em USD $" + USD);
+   alert ("Seu número de WONS em EUR €" + EUR);
 }; 
